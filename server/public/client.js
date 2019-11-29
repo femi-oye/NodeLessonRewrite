dd440e5b8c7df524d201d786d00b5522026d472b
Femi Oye <femoye@Femis-MacBook-Pro-16.local>
2019-11-29 06:56:20 -0600

    initial commit of working version

---
+++ server/public/client.js
@@ -0,0 +1,24 @@
+$(document).ready(readyNow);
+
+function readyNow() {
+    console.log('JQ loaded');
+
+    $.ajax({
+        method: 'GET',
+        url: '/food-items'
+    }).then(function(response) {
+        console.log(response);
+
+        let foodItems = response;
+
+        for (let item of foodItems) {
+            $('#displayFoodTable').append(`
+            <tr>
+                <td>${item.name}</td>
+                <td>${item.food}</td>
+                <td>${item.price}</td>
+            </tr>
+            `);
+        }
+    });
+}
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 5001;
+
+const foodItems = [{name: 'Bode', food: 'Eba', price: '$5'},
+                    {name: 'Femi', food: 'Crackers', price: '$2'},
+                    {name: 'Wasiu', food: 'Garri', price: '$10'}];
+
+app.use(express.static('server/public'));
+
+app.get('/food-items', (req, res) => {
+    res.send(foodItems);
+    console.log(foodItems);
+});
+
+app.listen(PORT, function() {
+    console.log(`listening on port ${PORT}`);
+});
